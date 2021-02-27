948c760ac4eb82ff928abe69e74020ce49648d03
Dante Ventura <dante.g.ventura@gmail.com>
2021-02-27 14:22:37 -0600

    discordjs example

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.login('ODE1MjkwNDg3ODc1NDM2NTc2.YDqQbw.M8jUAUhHMHOT2rZ0M2aMt7fl_GY')
