9c1c39b3e2ef24010e94db2a2ae7da6775e7b227
Dante Ventura <dante.g.ventura@gmail.com>
2021-02-27 17:04:28 -0600

    stuff

--- main.js
+++ main.js
@@ -4,7 +4,29 @@ const client = new Discord.Client()
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     client.guilds.cache.forEach(server => {
-        //var welcomeChannel = server.
+        let welcomeChannel = server.channels.cache.find(chnl => chnl.name === 'welcome' && chnl.type === 'text')
+
+        if (welcomeChannel) {
+            welcomeChannel.messages.fetch({ limit: 1 })
+            .then(messages => {
+                let lastMsg = messages.first()
+
+                if (!lastMsg.author.bot) {
+                    let roleEmbed = new Discord.MessageEmbed({
+                        color: '#e87500',
+                        description: 'Hello! Before you can view the server, you need to enter your name and select your pronouns. Please type your name and then react to your preferred pronouns.\n:one: - she/her\n:two: - he/him\n:three: - they/them\n:four: - they/she\n:five: - they/him\n:six: - any',
+                        
+                    })
+                    welcomeChannel.send(roleEmbed)
+                    .then(msg => {
+                        //msg.react()
+                    }).catch(console.error)
+                }         
+                else
+                    console.log('bad')
+
+            }).catch(console.error)
+        }
     })
 })
 
@@ -20,7 +42,7 @@ client.on('message', msg => {
             break
         case '!help':
             //send rich message with bot usages
-            //msg.reply(embed)
+            msg.reply('test')
             break
         default:
             break
