07e2616095826acbc5eca5322cccedbe44df81f4
Dante Ventura <dante.g.ventura@gmail.com>
2021-02-27 16:00:07 -0600

    stuff

--- main.js
+++ main.js
@@ -2,13 +2,29 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.guilds.cache.forEach(server => {
+        //var welcomeChannel = server.
+    })
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
+    //command: !setup
+    //!stuff parameter
+
+    var command = msg.content.split(' ')
+    //return command[] = {!stuff, parameter}
+
+    switch (command[0]) {
+        case '!setup':
+            break
+        case '!help':
+            //send rich message with bot usages
+            //msg.reply(embed)
+            break
+        default:
+            break
+    }
 })
 
 client.login('ODE1MjkwNDg3ODc1NDM2NTc2.YDqQbw.M8jUAUhHMHOT2rZ0M2aMt7fl_GY')
